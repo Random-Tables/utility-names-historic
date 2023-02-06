256403b4567ef8c7f9457c93b869c0cbc21c4677
aron edwards <aronedwards91@gmail.com>
2023-02-06 18:39:17 +0000

    Add generated calls

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const index = require("./index.json");
 const angloSaxon = require("./anglo-saxon.json");
+const calls = require("./calls.json");
 
 module.exports = {
   ...index,
   tableData: {
     angloSaxon,
   },
+  calls,
 };
